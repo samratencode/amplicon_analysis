2504eef980e5f7f7d4501fccc271b3080f21bca1
Samrat Ghosh <30471903+samratencode@users.noreply.github.com>
2023-08-27 18:00:41 +0200

    Update exporting.sh

--- exporting.sh
+++ exporting.sh
@@ -1,4 +1,4 @@
-#for i in *.qza; do
-#qiime tools export --input-path $i --output-path .
-#done
-#biom convert -i feature-table.biom -o feature-table.tsv --to-tsv
+for i in *.qza; do
+qiime tools export --input-path $i --output-path .
+done
+biom convert -i feature-table.biom -o feature-table.tsv --to-tsv
