e01eccdb3de88954611f40d20db95d85217afe8e
Samrat Ghosh <30471903+samratencode@users.noreply.github.com>
2025-02-15 18:49:26 +0100

    Update amplicon_import_silva_db_wf.sh

--- amplicon_import_silva_db_wf.sh
+++ amplicon_import_silva_db_wf.sh
@@ -1,3 +1,3 @@
-qiime tools import --type 'FeatureData[Sequence]' --input-path ./rep_set/rep_set_16S_only/99/silva_132_99_16S.fna --output-path silva_ver132_99_seqs_bac.qza
-qiime tools import --type 'FeatureData[Taxonomy]' --input-format HeaderlessTSVTaxonomyFormat --input-path ./taxonomy/16S_only/99/majority_taxonomy_7_levels.txt --output-path silva_ver132_99_tax_bac.qza
+qiime tools import --type 'FeatureData[Sequence]' --input-path ./rep_set/rep_set_16S_only/99/silva_138_99_16S.fna --output-path silva_ver138_99_seqs_bac.qza
+qiime tools import --type 'FeatureData[Taxonomy]' --input-format HeaderlessTSVTaxonomyFormat --input-path ./taxonomy/16S_only/99/majority_taxonomy_7_levels.txt --output-path silva_ver138_99_tax_bac.qza
 qiime feature-classifier fit-classifier-naive-bayes --i-reference-reads ref-seqs.qza  --i-reference-taxonomy ref-tax.qza --o-classifier silva_ver138_1_99_classifier_bac.qza
